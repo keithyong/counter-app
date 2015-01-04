161eb19f1db6715e93f19654144c791802653e8d
Keith Yong <terda12@gmail.com>
2015-01-04 09:49:08 -0500

    Initial Commit

---
+++ counter/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  counter
+//
+//  Created by Keith Yong on 1/4/15.
+//  Copyright (c) 2015 keith. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ counter/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  counter
+//
+//  Created by Keith Yong on 1/4/15.
+//  Copyright (c) 2015 keith. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
